01488746dd6c7a5b5cd55c4c6659cc087edcbab8
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-14 01:17:56 +0300

    140223-01:17

--- src/index.js
+++ src/index.js
@@ -38,8 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+    let arr = [];
+    let res = '';
+    
+    for (let i = 0; i < expr.length; i += 10){ // создаем массив, из элементов по 10 символов
+        arr.push(expr.substr(i, 10)); // метод substr() позволяет извлечь из строки определенное количество символов, начиная с заданного индекса
+    };
+
+    arr.forEach(el =>{
+        let toMorse = '';
+        for (let i = 0; i < el.length / 2; i++){
+            if(el.substr(i * 2, 2) === '10'){
+                toMorse += '.';
+            }else if(el.substr(i * 2, 2) === '11'){
+                toMorse += '-';
+            };
+        };
+
+        Object.keys(MORSE_TABLE).forEach(key =>{ // сравниваем строку toMorse с ключами объекта MORSE_TABLE
+            if(toMorse === key){
+                res += MORSE_TABLE[key]; 
+            };
+        });
+        
+        if(toMorse === ''){
+            res += ' ';
+        }; 
+    });
+
+    return res;
+};
 
 module.exports = {
     decode
